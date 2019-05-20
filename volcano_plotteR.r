51370ca7d9226c5f9de40b7109ce38f2ed63de5b
Jeffrey Grover <groverj3@gmail.com>
2019-05-20 00:20:25 -0700

    Cleanup

--- volcano_plotteR.r
+++ volcano_plotteR.r
@@ -1,15 +1,15 @@
 # Author: Jeffrey Grover
 # Purpose: Volcano PlotteR is an R script to generate volcano plots
 # Created: 2019-04-19
-# Usage: Load the results output from DESeq2 as a data frame, the
-# input data must have columns for log2FoldChange and padj
+# Usage: Load the results output from DESeq2 as a data frame, the input data
+# must have columns for log2FoldChange and padj
 
 library(dplyr)
 library(ggplot2)
 
 # Load this function and the above libraries
 
-volcplot <- function(data, sig, fc, out_prefix, format) {
+volcplot <- function(data, padj_threshold, fc, out_prefix, format) {
 
   # Set the fold-change thresholds
 
@@ -19,11 +19,12 @@ volcplot <- function(data, sig, fc, out_prefix, format) {
   # Make a dataset for plotting, add the status as a new column
 
   plot_ready_data <- na.omit(data) %>%
-    mutate(log2fc_threshold = ifelse((log2FoldChange >= pos_log2fc) & (padj <= sig), 'up',
-                              ifelse((log2FoldChange <= neg_log2fc) & (padj <= sig), 'down',
-                                     'unchanged')
+    mutate(log2fc_threshold = ifelse((log2FoldChange >= pos_log2fc) & (padj <= padj_threshold), 'up',
+                              ifelse((log2FoldChange <= neg_log2fc) & (padj <= padj_threshold), 'down',
+                                     'unchanged'
+                                     )
                                     )
-          )
+           )
 
   # Get the number of up, down, and unchanged genes
 
@@ -33,20 +34,23 @@ volcplot <- function(data, sig, fc, out_prefix, format) {
 
   # Make the labels for the legend
 
-  legend_labels <- c(str_c('Down: ', down_genes), str_c('Unchanged: ', unchanged_genes),
+  legend_labels <- c(str_c('Down: ', down_genes),
+                     str_c('Unchanged: ', unchanged_genes),
                      str_c('Up: ', up_genes)
                     )
 
   # Set the x axis limits, rounded to the next even number
 
   x_axis_limits <- RoundTo(
-    log2(max(log2FoldChange)), 2, ceiling
+    log2(max(abs(log2FoldChange))), 2, ceiling
   )
-  x_axis_limits <- c(-x_axis_limits, x_axis_limits)
 
   # Set the plot colors
 
-  plot_colors <- c('down' = 'dodgerblue1', 'unchanged' = 'gray', 'up' = 'firebrick1')
+  plot_colors <- c('down' = 'dodgerblue1',
+                   'unchanged' = 'gray',
+                   'up' = 'firebrick1'
+                   )
 
   # Make the plot, these options are a reasonable strting point
 
@@ -55,7 +59,7 @@ volcplot <- function(data, sig, fc, out_prefix, format) {
     aes(x=log2FoldChange, y = -log10(padj), color = log2fc_threshold) +
     geom_vline(xintercept = c(neg_log2fc, pos_log2fc), linetype = 'dotted') +
     geom_hline(yintercept = -log10(sig), linetype = 'dotted') +
-    scale_x_continuous('log2(FC)', limits = x_axis_limits) +
+    scale_x_continuous('log2(FC)', limits = c(-x_axis_limits, x_axis_limits)) +
     scale_color_manual(values = plot_colors, labels = legend_labels) +
     labs(color = str_c(fc, '-fold, padj ≤', sig)) +
     theme(aspect.ratio = 1) +
