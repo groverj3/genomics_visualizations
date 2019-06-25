2f9587f0ade778248850be36adc628c72569983d
Jeffrey Grover <groverj3@gmail.com>
2019-06-25 00:21:33 -0700

    Refactored, needs testing

--- metaplotteR.r
+++ metaplotteR.r
@@ -1,28 +1,111 @@
+#!/usr/bin/env Rscript 
+
 # Author: Jeffrey Grover
 # Purpose: MetaplotteR creates metaplots of data over genomic features
-# Created: 2019-05-21
-# Usage: Load the data per bin into a data frame. This data can be in the form of
-# coverage, percent methylation, etc. The input data frame should have columns
-# for bin, value, and sample_id and there should be 60 bins with a value to plot
+# Created: 2019-06-24
+# Usage: Reads the table from deeptools' metaplot and makes a prettier one
 
-library(dplyr)
-library(ggplot2)
+library(argparser, quietly=TRUE)
+suppressMessages(library(dplyr))
+suppressMessages(library(ggplot2))
 
-# Load this function and the above libraries
 
-metaplot <- function(data, window_size, x_labels, y_axis_label, out_prefix, format) {
+# Subroutine functions
 
-  # For now the plot labels at 16 discrete intervals, so the x_labels vector must
-  # also be 16 items in length. Position 6 corresponds to the start of the
-  # feature, and 11 corresponds to the end of the feature.
- 
-  ggplot(data, aes(x = bin, y = value, group = genotype, color = genotype)) +
-    geom_smooth(span = 0.2, se = FALSE) +
-    scale_x_continuous(breaks = c(0,4,8,12,16,20,24,28,32,36,40,44,48,52,56,60),
-                       labels = x_labels) +
-    geom_vline(xintercept = c(20, 40), linetype = 'dotted') +
+read_deeptools_table <- function(file, header=TRUE, sep='\t', ...) {
+
+  n <- max(count.fields(file, sep = sep), na.rm = TRUE)
+  x <- readLines(file)
+
+  .splitvar <- function(x, sep, n) {
+    var <- unlist(strsplit(x, split = sep))
+    length(var) <- n
+    return(var)
+  }
+
+  x <- do.call(cbind, lapply(x, .splitvar, sep = sep, n = n))
+  x <- apply(x, 1, paste, collapse = sep) 
+  plot_table <- read.csv(text = x, sep = sep, header = header, ...)
+  plot_table <- plot_table[-1,]  # Remove first row
+
+  return(plot_table)
+}
+
+
+table_to_long <- function(plot_table) {
+
+  long_table <- gather(plot_table, 'sample', 'score', -bin.labels, -bins)
+  return(long_table)
+}
+
+
+metaplot <- function(plot_table, start_label, end_label, y_axis_label, span
+                     out_prefix, format) {
+
+  start_bin <- filter(plot_table, bin.labels == start_label)$bins
+  end_bin <- filter(plot_table, bin.labels == end_label)$bins
+
+  ggplot(data) +
+    aes(x = bins, y = score, group = sample, color = sample) +
+    geom_smooth(span = span, se = FALSE) +
+    scale_x_continuous(labels = bin.labels) +
+    geom_vline(xintercept = c(start_bin, end_bin), linetype = 'dotted') +
     ylab(y_axis_label) +
     xlab('Position') +
-    theme(legend.title = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1)) +
-    ggsave(str_c(out_prefix, '_metaplot_', window_size, 'nt_window.', format))
+    theme_bw(base_size = '24') +
+    theme(legend.title = element_blank(),
+          axis.text.x = element_text(angle = 45, hjust = 1)) +
+    ggsave(paste0(out_prefix, '_metaplot.', format))
 }
+
+
+# Parse arguments
+
+get_args <- function() {
+
+  parser <- arg_parser(
+    paste0('metaplotteR will read a table output from deeptools\' plotProfile ',
+           'command, load it into R, and make a new metaplot with ggplot!')
+  )
+  parser <- add_argument(parser, c('-o', '--output_prefix'),
+                       type='integer',
+                       default='plot',
+                       help='Prefix to add to output plot',
+                       metavar='path/filename')
+  parser <- add_argument(parser, c('-s', '--start_label'),
+                       default='start',
+                       help='Label for the start of the features, must match the table.')
+  parser <- add_argument(parser, c('-e', '--end_label'),
+                       default='end',
+                       help='Label for the end of the features, must match the table.')
+  parser <- add_argument(parser, c('-y', '--y_axis'),
+                       default='coverage',
+                       help='Label (units) for the y-axis')
+  parser <- add_argument(parser, c('-n', '--span'),
+                       type='numeric',
+                       default=0.2,
+                       help='Value for span in geom_smooth, smaller values are wigglier.')
+  parser <- add_argument(parser, c('-f', '--format'),
+                       default='png',
+                       help='Image format. [png|svg|jpeg|pdf|bmp|tiff|eps|ps]')
+  parser <- add_argument(parser, 'input_file',
+                         help='Deeptools plotProfile table',
+                         metavar='File')
+  return(parse_args(parser))
+}
+
+
+# Main function entry point
+
+
+main <- function(args=get_args()) {
+
+  deeptools_table <- read_deeptools_table(args$input_file)
+
+  deeptools_table <- table_to_long(deeptools_table)
+
+  metaplot(deeptools_table, args$start_label, args$end_label, args$y_axis,
+           args$span, args$out_prefix, args$format)
+}
+
+main()
