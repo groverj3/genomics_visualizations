6a6848f6b7f50316fa68aeb710db6586360da262
Jeffrey Grover <jeffrey.w.grover@gmail.com>
2024-02-05 16:06:31 -0500

    Updated volcano plot graph since this is still better than most of the code out there that I've seen for these

--- volcano_plotteR.r
+++ volcano_plotteR.r
@@ -9,7 +9,7 @@ library(ggplot2)
 
 # Load this function and the above libraries
 
-volcplot <- function(data, padj_threshold, fc) {
+volcplot <- function(data, padj_threshold, fc, plot_title, plot_subtitle) {
 
   # Set the fold-change thresholds
 
@@ -18,10 +18,12 @@ volcplot <- function(data, padj_threshold, fc) {
 
   # Make a dataset for plotting, add the status as a new column
 
-  plot_ready_data <- na.omit(data) %>%
+  plot_ready_data <- data %>% mutate_at('padj', ~replace(.x, is.na(.x), 1))
     mutate(
         log2fc_threshold = ifelse((log2FoldChange >= pos_log2fc) & (padj <= padj_threshold), 'up',
-                                  ifelse((log2FoldChange <= neg_log2fc) & (padj <= padj_threshold), 'down','ns'))
+                           ifelse((log2FoldChange <= neg_log2fc) & (padj <= padj_threshold), 'down',
+                           'ns')
+                           )
     )
 
   # Get the number of up, down, and unchanged genes
@@ -41,7 +43,9 @@ volcplot <- function(data, padj_threshold, fc) {
   # Set the x axis limits, rounded to the next even number
 
   x_axis_limits <- DescTools::RoundTo(
-    max(abs(plot_ready_data$log2FoldChange)), 2, ceiling
+    max(abs(plot_ready_data$log2FoldChange)),
+    2,
+    ceiling
   )
 
   # Set the plot colors
@@ -56,17 +60,42 @@ volcplot <- function(data, padj_threshold, fc) {
   # Make the plot, these options are a reasonable strting point
 
   ggplot(plot_ready_data) +
-    geom_point(alpha = 0.25, size = 1.5) +
-    aes(x = log2FoldChange, y = -log10(padj), color = log2fc_threshold) +
-    geom_vline(xintercept = c(neg_log2fc, pos_log2fc), linetype = 'dashed') +
-    geom_hline(yintercept = -log10(padj_threshold), linetype = 'dashed') +
-    scale_x_continuous('log2(FC)', limits = c(-x_axis_limits, x_axis_limits)) +
-    scale_color_manual(values = plot_colors, labels = legend_labels) +
-    labs(color = str_c(fc, '-fold, padj ≤', padj_threshold)) +
+    geom_point(
+      alpha = 0.25,
+      size = 1.5
+    ) +
+    aes(
+      x = log2FoldChange,
+      y = -log10(padj),
+      color = log2fc_threshold
+    ) +
+    geom_vline(
+      xintercept = c(neg_log2fc, pos_log2fc),
+      linetype = 'dashed'
+    ) +
+    geom_hline(
+      yintercept = -log10(padj_threshold),
+      linetype = 'dashed'
+    ) +
+    scale_x_continuous(
+      'log2(FC)',
+      limits = c(-x_axis_limits, x_axis_limits)
+    ) +
+    scale_color_manual(
+      values = plot_colors,
+      labels = legend_labels
+      ) +
+    labs(
+      color = str_c(fc, '-fold, padj ≤', padj_threshold),
+      title = plot_title,
+      subtitle = plot_subtitle
+    ) +
     theme_bw(base_size = 24) +
-    theme(aspect.ratio = 1,
-          axis.text = element_text(color = 'black'),
-          legend.margin = margin(0,0,0,0),
-          legend.box.margin = margin(-10,-10,-10,-10),  # Reduces dead area around legend
-          legend.spacing.x = unit(0.2, 'cm'))
+    theme(
+      aspect.ratio = 1,
+      axis.text = element_text(color = 'black'),
+      legend.margin = margin(0,0,0,0),
+      legend.box.margin = margin(-10,-10,-10,-10),  # Reduces dead area around legend
+      legend.spacing.x = unit(0.2, 'cm')
+    )
 }
